150c4287c244090589520759d971078222e226ff
Nickolay Troshkin <nicktrs@mail.ru>
2023-04-18 18:23:39 +0300

    Пробросить исключение в UserService GL-19

--- application/server/src/main/java/com/journey/server/service/UserService.java
+++ application/server/src/main/java/com/journey/server/service/UserService.java
@@ -1,7 +1,10 @@
 package com.journey.server.service;
 
 import com.journey.server.entity.UserEntity;
+import com.journey.server.exceptions.LoginConflictException;
 import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
 
 @Service
 public class UserService {
@@ -15,7 +18,7 @@ public class UserService {
         return repo.getUserById(id);
     }
 
-    public int register(UserEntity user) {
+    public int register(UserEntity user) throws SQLException, LoginConflictException {
         return repo.createUser(user);
     }
 }
