54a23580b105e3bcda9e24437678e4d4a689a367
Nickolay Troshkin <nicktrs@mail.ru>
2023-04-18 18:21:34 +0300

    Добавить метод создания нового пользователя в БД GL-19

--- application/server/src/main/java/com/journey/server/service/IUserRepo.java
+++ application/server/src/main/java/com/journey/server/service/IUserRepo.java
@@ -1,9 +1,12 @@
 package com.journey.server.service;
 
 import com.journey.server.entity.UserEntity;
+import com.journey.server.exceptions.LoginConflictException;
+
+import java.sql.SQLException;
 
 public interface IUserRepo {
     UserEntity getUserById(int id);
 
-    int createUser(UserEntity user);
+    int createUser(UserEntity user) throws LoginConflictException, SQLException;
 }
