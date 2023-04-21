42301ec556e12f864a25fe1ccf17c92225806b8d
Nickolay Troshkin <nicktrs@mail.ru>
2023-04-21 16:01:40 +0300

    Добавить методы генерации токенов в сервис авторизации GL-15

--- application/server/src/main/java/com/journey/server/service/UserService.java
+++ application/server/src/main/java/com/journey/server/service/UserService.java
@@ -6,6 +6,8 @@ import com.journey.server.exceptions.WrongPasswordException;
 import com.journey.server.jwt.JwtProvider;
 import com.journey.server.jwt.JwtRequestDTO;
 import com.journey.server.jwt.JwtResponseDTO;
+import io.jsonwebtoken.Claims;
+import jakarta.security.auth.message.AuthException;
 import lombok.NonNull;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +18,7 @@ import java.util.Map;
 @Service
 public class UserService {
     private final IUserRepo repo;
-    private final Map<String, String> refreshStorage = new HashMap<>();
+    private final Map<Integer, String> refreshStorage = new HashMap<>();
     private final JwtProvider jwtProvider;
 
     public UserService(IUserRepo repo, JwtProvider provider) {
@@ -43,9 +45,42 @@ public class UserService {
         UserEntity user = repo.getUserById(id);
         String accessToken = jwtProvider.generateAccessToken(user);
         String refreshToken = jwtProvider.generateRefreshToken(user);
-        refreshStorage.put(user.getLogin(), refreshToken);
+        refreshStorage.put(user.getId(), refreshToken);
 
         return new JwtResponseDTO(accessToken, refreshToken);
+    }
 
+    public JwtResponseDTO getAccessToken(@NonNull String refreshToken) throws SQLException {
+        if (jwtProvider.validateRefreshToken(refreshToken)) {
+            final Claims claims = jwtProvider.getRefreshClaims(refreshToken);
+            final int id = claims.get("id", Integer.class);
+            final String saveRefreshToken = refreshStorage.get(id);
+
+            if (saveRefreshToken != null && saveRefreshToken.equals(refreshToken)) {
+                final UserEntity user = repo.getUserById(id);
+                final String accessToken = jwtProvider.generateAccessToken(user);
+                return new JwtResponseDTO(accessToken, null);
+            }
+        }
+
+        return new JwtResponseDTO(null, null);
+    }
+
+    public JwtResponseDTO refresh(@NonNull String refreshToken) throws AuthException, SQLException {
+        if (jwtProvider.validateRefreshToken(refreshToken)) {
+            final Claims claims = jwtProvider.getRefreshClaims(refreshToken);
+            final int id = claims.get("id", Integer.class);
+            final String saveRefreshToken = refreshStorage.get(id);
+
+            if (saveRefreshToken != null && saveRefreshToken.equals(refreshToken)) {
+                final UserEntity user = repo.getUserById(id);
+                final String accessToken = jwtProvider.generateAccessToken(user);
+                final String newRefreshToken = jwtProvider.generateRefreshToken(user);
+
+                refreshStorage.put(user.getId(), newRefreshToken);
+                return new JwtResponseDTO(accessToken, newRefreshToken);
+            }
+        }
+        throw new AuthException("Невалидный JWT токен");
     }
 }
