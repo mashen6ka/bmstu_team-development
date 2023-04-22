a8d8a3dacd50adbbe6ffc8321089c8dcfa0108fe
Nickolay Troshkin <nicktrs@mail.ru>
2023-04-22 19:36:34 +0000

    Реализовать интеграционные тесты для контроллера мест GL-42

---
+++ application/server/src/test/java/com/journey/server/integration/PlaceControllerITCase.java
@@ -0,0 +1,197 @@
+package com.journey.server.integration;
+
+import com.journey.server.controller.PlaceController;
+import com.journey.server.dto.place.CreatePlaceDTO;
+import com.journey.server.dto.place.FullInfoPlaceDTO;
+import com.journey.server.entity.PlaceEntity;
+import com.journey.server.utils.PlaceObjectMother;
+import com.journey.server.utils.PropertiesUtil;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static com.journey.server.utils.Assertions.assertDTOEquals;
+import static com.journey.server.utils.PlaceObjectMother.*;
+import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PlaceControllerITCase {
+    @Autowired
+    private PlaceController controller;
+
+    @Before
+    public void setUp() throws IOException, InterruptedException {
+        String[] cmd = {"/bin/sh", "-c", "cd " + System.getProperty("user.dir")
+                        + "/../sql && ./prepare_data.sh " + PropertiesUtil.get("db.password") + " && cd ../server"};
+        Process p = Runtime.getRuntime().exec(cmd);
+        p.waitFor();
+    }
+
+    @After
+    public void tearDown() throws IOException, InterruptedException {
+        String[] cmd = {"/bin/sh", "-c", "cd " + System.getProperty("user.dir")
+                        + "/../sql && ./rollback_data.sh " + PropertiesUtil.get("db.password") + " && cd ../server"};
+        Process p = Runtime.getRuntime().exec(cmd);
+        p.waitFor();
+    }
+
+    @Test
+    public void getPlaceListByUserLengthITCase() throws SQLException {
+        // act
+        ArrayList<FullInfoPlaceDTO> res = controller.getPlaceListByUserId(4);
+
+        // assert
+        assertEquals(res.size(), 3);
+    }
+
+    @Test
+    public void getPlaceListByUserRightUserITCase() throws SQLException {
+        // act
+        ArrayList<FullInfoPlaceDTO> res = controller.getPlaceListByUserId(4);
+
+        // assert
+        for (FullInfoPlaceDTO dto : res)
+            assertEquals(dto.getAuthorName(), "Николай Трошкин");
+    }
+
+    @Test
+    public void getPlaceListByUserNoSuchUserITCase() throws SQLException {
+        // act
+        ArrayList<FullInfoPlaceDTO> res = controller.getPlaceListByUserId(100);
+
+        // assert
+        assertEquals(res.size(), 0);
+    }
+
+    @Test
+    public void getPlaceByIdITCase() throws Exception {
+        // arrange
+        FullInfoPlaceDTO outDTO = getSomeExistingOutputPlace();
+
+        // act
+        FullInfoPlaceDTO dto = controller.getPlaceById(12);
+
+        // assert
+        assertDTOEquals(dto, outDTO);
+    }
+
+    @Test
+    public void getPlaceByIdNoSuchPlaceITCase() throws Exception {
+        // act
+        FullInfoPlaceDTO dto = controller.getPlaceById(100);
+
+        // assert
+        assertNull(dto);
+    }
+
+    @Test
+    public void deletePlaceByIdITCase() throws Exception {
+        // act
+        controller.deletePlaceById(10);
+        FullInfoPlaceDTO dto = controller.getPlaceById(10);
+
+        // assert
+        assertNull(dto);
+    }
+
+    @Test
+    public void deletePlaceByIdNoSuchPlaceITCase() throws Exception {
+        // act
+        controller.deletePlaceById(100);
+    }
+
+    @Test
+    public void createPlaceITCase() throws Exception {
+        // arrange
+        CreatePlaceDTO inDTO = getSomeNewInputPlace();
+        FullInfoPlaceDTO outDTO = getSomeNewOutputPlace();
+
+        // act
+        ResponseEntity<String> response = controller.createPlace(inDTO);
+
+        // assert
+        assertEquals(response.getStatusCode(), HttpStatus.CREATED);
+
+        // act
+        FullInfoPlaceDTO dto = controller.getPlaceById(31);
+
+        // assert
+        assertDTOEquals(dto, outDTO);
+    }
+
+    @Test
+    public void updatePlaceByIdITCase() throws Exception {
+        // arrange
+        FullInfoPlaceDTO initDTO = getSomeExistingOutputPlace();
+        CreatePlaceDTO inDTO = getSomeNewInputPlace();
+        FullInfoPlaceDTO outDTO = getSomeNewOutputPlace();
+
+        // act
+        FullInfoPlaceDTO dto = controller.getPlaceById(12);
+
+        // assert
+        assertDTOEquals(dto, initDTO);
+
+        // act
+        controller.updatePlace(12, inDTO);
+        dto = controller.getPlaceById(12);
+
+        // assert
+        assertDTOEquals(dto, outDTO);
+    }
+
+    @Test
+    public void updatePlaceByIdNoSuchPlaceITCase() throws Exception {
+        // arrange
+        CreatePlaceDTO inDTO = getSomeNewInputPlace();
+
+        // act
+        controller.updatePlace(100, inDTO);
+        FullInfoPlaceDTO dto = controller.getPlaceById(100);
+
+        // assert
+        assertNull(dto);
+    }
+
+    @Test
+    public void updateIsVisitedITCase() throws Exception {
+        // arrange
+        FullInfoPlaceDTO initDTO = getSomeExistingOutputPlace();
+        FullInfoPlaceDTO outDTO = getSomeExistingOutputPlace();
+        outDTO.setVisited(true);
+
+        // act
+        FullInfoPlaceDTO dto = controller.getPlaceById(12);
+
+        // assert
+        assertDTOEquals(dto, initDTO);
+
+        // act
+        controller.updateIsVisited(12, true);
+        dto = controller.getPlaceById(12);
+
+        // assert
+        assertDTOEquals(dto, outDTO);
+    }
+
+    @Test
+    public void updateIsVisitedNoSuchPlaceITCase() throws Exception {
+        // act
+        controller.updateIsVisited(100, true);
+        FullInfoPlaceDTO dto = controller.getPlaceById(100);
+
+        // assert
+        assertNull(dto);
+    }
+}
---
+++ application/server/src/test/java/com/journey/server/utils/Assertions.java
@@ -0,0 +1,14 @@
+package com.journey.server.utils;
+
+import static org.junit.jupiter.api.Assertions.*;
+import com.journey.server.dto.place.FullInfoPlaceDTO;
+
+public class Assertions {
+    public static void assertDTOEquals(FullInfoPlaceDTO dto1, FullInfoPlaceDTO dto2) {
+        assertEquals(dto1.getAuthorName(), dto2.getAuthorName());
+        assertEquals(dto1.getTitle(), dto2.getTitle());
+        assertEquals(dto1.isVisited(), dto2.isVisited());
+        assertEquals(dto1.getDttmUpdate(), dto2.getDttmUpdate());
+        assertEquals(dto1.getCardText(), dto2.getCardText());
+    }
+}
