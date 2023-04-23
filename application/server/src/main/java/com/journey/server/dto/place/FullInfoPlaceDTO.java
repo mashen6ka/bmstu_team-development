d2c59eb5e09bd10ae30a799ddd7fc83b074aa9e3
Nickolay Troshkin <nicktrs@mail.ru>
2023-04-23 23:57:33 +0300

    Добавить комментарии к сущностям DTO GL-47

--- application/server/src/main/java/com/journey/server/dto/place/FullInfoPlaceDTO.java
+++ application/server/src/main/java/com/journey/server/dto/place/FullInfoPlaceDTO.java
@@ -3,12 +3,34 @@ package com.journey.server.dto.place;
 import lombok.Builder;
 import lombok.Data;
 
+/**
+ * Сущность DTO, содержащая информацию о месте, которая возвращается пользователю при GET-запросах
+ */
 @Builder
 @Data
 public class FullInfoPlaceDTO {
+    /**
+     * Имя пользователя, создавшего карточку места
+     */
     String authorName;
+
+    /**
+     * Флаг посещенности места (true - посещенное, false - желаемое)
+     */
     boolean isVisited;
+
+    /**
+     * Заголовок карточки места
+     */
     String title;
+
+    /**
+     * Подробное описание карточки места
+     */
     String cardText;
+
+    /**
+     * Время последнего обновления карточки места (в формате YYYY-MM-DD HH24:MI)
+     */
     String dttmUpdate;
 }
--- application/server/src/main/java/com/journey/server/dto/user/RegistryUserDTO.java
+++ application/server/src/main/java/com/journey/server/dto/user/RegistryUserDTO.java
@@ -3,10 +3,24 @@ package com.journey.server.dto.user;
 import lombok.Builder;
 import lombok.Data;
 
+/**
+ * Сущность DTO, содержащая информацию о пользователе, которую тот указывает при регистрации
+ */
 @Builder
 @Data
 public class RegistryUserDTO {
+    /**
+     * Логин пользователя
+     */
     String login;
+
+    /**
+     * Хеш пароля пользователя
+     */
     String hash;
+
+    /**
+     * Имя пользователя
+     */
     String name;
 }
