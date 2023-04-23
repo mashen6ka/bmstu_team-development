0ba9bb88e876bcdc45feb6726682e174c4fe8d4d
Nickolay Troshkin <nicktrs@mail.ru>
2023-04-23 22:54:05 +0300

    Добавить комментарии к интерфейсам репозиториев GL-47

--- application/server/src/main/java/com/journey/server/service/IUserRepo.java
+++ application/server/src/main/java/com/journey/server/service/IUserRepo.java
@@ -6,10 +6,34 @@ import com.journey.server.exceptions.WrongPasswordException;
 
 import java.sql.SQLException;
 
+/**
+ * Интерфейс репозитория используется для работы с таблицей, отвечающей за пользователей в базе данных
+ */
 public interface IUserRepo {
+    /**
+     * Получение информации о пользователе по идентификатору
+     * @param id идентификатор пользователя, информацию о котором требуется получить
+     * @return объект UserEntity, содержащий информацию о пользователе
+     * @throws SQLException при неуспешном подключении или внутренней ошибке базы данных
+     */
     UserEntity getUserById(int id) throws SQLException;
 
+    /**
+     * Создание новой записи о пользователе в базе данных
+     * @param user сущность UserEntity, содержашая информацию о пользователе при его регистрации
+     * @return идентификатор, назначенный новому пользователю в базе данных
+     * @throws LoginConflictException если пользователь с указанным при регистрации логином уже занят
+     * @throws SQLException при неуспешном подключении или внутренней ошибке базы данных
+     */
     int createUser(UserEntity user) throws LoginConflictException, SQLException;
 
+    /**
+     * Проверка логина и пароля при входе
+     * @param login логин пользователя
+     * @param hash хеш пароля пользователя (SHA-256)
+     * @return идентификатор пользователя, если в БД есть запись с указанными логином и паролем
+     * @throws WrongPasswordException при несоответствии введенных логина и пароля
+     * @throws SQLException при неуспешном подключении или внутренней ошибке базы данных
+     */
     int authenticate(String login, String hash) throws WrongPasswordException, SQLException ;
 }
