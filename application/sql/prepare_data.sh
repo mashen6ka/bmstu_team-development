cc20d3900459c7ff905d981f58fc545491c9bc47
Ekaterina <ekaterinaamzr@gmail.com>
2023-04-25 21:45:34 +0300

    Убрать sudo при выполнении команд docker в скриптах БД #38

--- application/sql/prepare_data.sh
+++ application/sql/prepare_data.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # get_data
-sudo docker exec -i database sh -c "/sh/get_data.sh $1"
+docker exec -i database sh -c "/sh/get_data.sh $1"
--- application/sql/run_database.sh
+++ application/sql/run_database.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # copy db data
-sudo docker cp . database:/
+docker cp . database:/
 
 # generate tables
-sudo docker exec -i database sh -c "/sh/generate_db.sh $1"
+docker exec -i database sh -c "/sh/generate_db.sh $1"
 
