2e760938fb3cda55db08478b4a03c4d4c4cc896a
slepokurovamf <slepokurovamf@oooinex.ru>
2023-05-02 16:23:04 +0300

    Формировать хедер авторизации при инициализации vuex store GL-71

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -3,6 +3,11 @@ import Vuex from "vuex";
 
 import Auth from "./Auth";
 import Place from "./Place";
+import ax from "@/axios-instance";
+
+ax.defaults.headers.common["Authorization"] = `Bearer ${localStorage.getItem(
+  "accessToken"
+)}`;
 
 Vue.use(Vuex);
 
